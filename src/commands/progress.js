3014a6673089231efa894d69430dd115e41c2df2
markdstouffer <markdstouffer@gmail.com>
2021-07-15 00:00:36 -0400

    add tag-alias ability to progress & recent

--- src/commands/progress.js
+++ src/commands/progress.js
@@ -1,14 +1,27 @@
 const { request } = require('graphql-request')
 const { GET_MEDIA, GET_USERINFO, GET_MEDIALIST } = require('../queries')
+const { alias } = require('../aliases')
 
 module.exports = {
   name: 'progress',
   description: 'Returns how many episodes of an anime a user has watched, given a username and an anime title.',
 	usage: '[anilist username] [anime title]',
   async execute(msg, args) {
-      const title = args.splice(1, args.length).join(' ')
-      console.log('title', title)
-      const idData = await request('https://graphql.anilist.co', GET_MEDIA, {search: title})
+    const title = args.splice(1, args.length).join(' ')
+    const idData = await request('https://graphql.anilist.co', GET_MEDIA, {search: title})
+    if (args[0].startsWith('<')) {
+      const id = args[0].slice(3, args[0].length-1)
+      const user = alias.find(x => x.id === id)
+      const userData = await request('https://graphql.anilist.co', GET_USERINFO, {name: user.username})
+      try {
+        const listData = await request('https://graphql.anilist.co', GET_MEDIALIST, {userName: userData.User.name, mediaId: idData.Media.id})
+        msg.reply(`${userData.User.name} has completed ${listData.MediaList.progress} episode(s) of ${listData.MediaList.media.title.romaji}`)
+      } catch (error) {
+        console.log(error)
+        msg.reply(`${userData.User.name} has not yet watched any episodes of this anime.`)
+      }
+    }
+    else {
       const userData = await request('https://graphql.anilist.co', GET_USERINFO, {name: args[0]})
       try {
         const listData = await request('https://graphql.anilist.co', GET_MEDIALIST, {userName: userData.User.name, mediaId: idData.Media.id})
@@ -17,5 +30,6 @@ module.exports = {
         console.log(error)
         msg.reply(`${userData.User.name} has not yet watched any episodes of this anime.`)
       }
+    }
   }
 }
--- src/commands/recent.js
+++ src/commands/recent.js
@@ -1,12 +1,16 @@
 const { request } = require('graphql-request')
 const { GET_ACTIVITY, GET_USERINFO } = require('../queries')
+const { alias } = require('../aliases')
 
 module.exports = {
   name: 'recent',
   description: 'Return the most recent activity given a username.',
-	usage: '[username]',
+	usage: '[anilist username]',
   execute(msg, args) {
-    request('https://graphql.anilist.co', GET_USERINFO, {name: args[0]})
+    if (args[0].startsWith('<')) {
+      const id = args[0].slice(3, args[0].length-1)
+      const user = alias.find(x => x.id === id)
+      request('https://graphql.anilist.co', GET_USERINFO, {name: user.username})
         .then(res => {
           request('https://graphql.anilist.co', GET_ACTIVITY, {userId: res.User.id})
             .then(res2 => {
@@ -14,5 +18,16 @@ module.exports = {
               msg.reply(`${res.User.name} - ${activity.status} ${activity.progress} (${activity.media.title.romaji})`)
             })
         })
+    }
+    else {
+      request('https://graphql.anilist.co', GET_USERINFO, {name: args[0]})
+        .then(res => {
+          request('https://graphql.anilist.co', GET_ACTIVITY, {userId: res.User.id})
+            .then(res2 => {
+              const activity = res2.Page.activities[0]
+              msg.reply(`${res.User.name} - ${activity.status} ${activity.progress} (${activity.media.title.romaji})`)
+            })
+        })
+    }
   }
 }
