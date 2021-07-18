73db28cce8a9bc8a8aa50e40348fec24b0969ec5
markdstouffer <markdstouffer@gmail.com>
2021-07-18 16:25:07 -0400

    update 'score' to embed, including color gradient

--- src/commands/score.js
+++ src/commands/score.js
@@ -1,5 +1,6 @@
 const { request } = require('graphql-request')
 const { GET_MEDIA, GET_USERINFO, GET_MEDIALIST } = require('../queries')
+const Discord = require('discord.js')
 const path = require('path')
 const fs = require('fs')
 
@@ -11,6 +12,18 @@ module.exports = {
   async execute(msg, args) {
     let usersjson = fs.readFileSync(path.resolve(__dirname, '../data/alias.json'), 'utf-8')
     let usersArray = JSON.parse(usersjson)
+    function percentToHex(percent, start, end, s, l) {
+      l /= 100
+      const x = (percent/100), y = (end-start) * x, 
+        h = y + start
+      const a = s * Math.min(l, 1 - l) / 100;
+      const f = n => {
+        const k = (n + h / 30) % 12
+        const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1)
+        return Math.round(255 * color).toString(16).padStart(2, '0')
+      }
+      return `#${f(0)}${f(8)}${f(4)}`
+    } //function to convert percent to HEX (adapted from u/icl7126, u/Mattisdada)
     try {
       const title = args.splice(1, args.length).join(' ')
       const idData = await request('https://graphql.anilist.co', GET_MEDIA, {search: title})
@@ -20,9 +33,16 @@ module.exports = {
         const userData = await request('https://graphql.anilist.co', GET_USERINFO, {name: username})
         try {
           const listData = await request('https://graphql.anilist.co', GET_MEDIALIST, {userName: userData.User.name, mediaId: idData.Media.id})
-          msg.reply(`${userData.User.name} rated **${listData.MediaList.media.title.romaji}** a **${listData.MediaList.score}**`)
-        } catch (error) {
-          console.log(error)
+          const score = listData.MediaList.score * 10
+          const color = percentToHex(score, 0, 110, 100, 50)
+          const embed = new Discord.MessageEmbed()
+            .setColor(color)
+            .setThumbnail(idData.Media.coverImage.large)
+            .setDescription(`[**${idData.Media.title.romaji}**](${idData.Media.siteUrl})`)
+            .setTitle(userData.User.name)
+            .addField('Score:', `${score/10}/10`)
+          msg.reply(embed)
+        } catch {
           msg.reply(`${userData.User.name} has not yet rated this anime.`)
         }
       }
@@ -30,9 +50,17 @@ module.exports = {
         const userData = await request('https://graphql.anilist.co', GET_USERINFO, {name: args[0]})
         try {
           const listData = await request('https://graphql.anilist.co', GET_MEDIALIST, {userName: userData.User.name, mediaId: idData.Media.id})
-          msg.reply(`${userData.User.name} rated **${listData.MediaList.media.title.romaji}** a **${listData.MediaList.score}**`)
-        } catch (error) {
-          console.log(error)
+          const score = listData.MediaList.score * 10
+          const color = percentToHex(score, 0, 110, 100, 50)
+          console.log(color)
+          const embed = new Discord.MessageEmbed()
+            .setColor(color)
+            .setThumbnail(idData.Media.coverImage.large)
+            .setDescription(`[**${idData.Media.title.romaji}**](${idData.Media.siteUrl})`)
+            .setTitle(userData.User.name)
+            .addField('Score:', `${score/10}/10`)
+          msg.reply(embed)
+        } catch {
           msg.reply(`${userData.User.name} has not yet rated this anime.`)
         }
     }
