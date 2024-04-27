44a7625ca0594bc0f2c7e3d8849d2ca57c17cdd0
Mark Stouffer <markdstouffer@gmail.com>
2024-04-26 20:57:41 -0400

    test commit

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -6,6 +6,6 @@ module.exports = {
     .setName('ping')
     .setDescription('Responds with pong.'),
   async execute (interaction: CommandInteraction) {
-    await interaction.reply('Pong pong pong!!')
+    await interaction.reply('Pong pong pong.')
   }
 }
